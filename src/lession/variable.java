0391719bb067f461b0937cf0f61837216f765c21
Linh Le <yenlinhit.lt@gmail.com>
2024-01-10 22:22:14 +0700

    init commit

---
+++ src/lession/variable.java
@@ -0,0 +1,14 @@
+package lession;
+
+public class variable {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        long myNumb = 10L;
+        float myFloatNum = 3.14f;
+        double myDouble = 3.14d;
+        //byte, short, int, long
+        System.out.println(Short.MAX_VALUE);
+        //constant variable - convention
+        final float PI_NUMBER = 3.14f;
+    }
+}
